4f32db7e8f760e5491c8029fbe1de76d25c9f384
Vishal Kumar <145376154+vishalrathor001@users.noreply.github.com>
2026-04-06 06:19:34 +0530

    Fix import case for UserDashboard component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import UserDashboard from '../components/userDashboard'
+import UserDashboard from '../components/UserDashboard'
 import OwnerDashboard from '../components/OwnerDashboard'
 import DeliveryBoy from '../components/DeliveryBoy'
 
