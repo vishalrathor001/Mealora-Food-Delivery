981cfe3b99c6c4c5b07d828b2f6f1a6b6721056a
Vishal Kumar <145376154+vishalrathor001@users.noreply.github.com>
2026-04-06 06:28:34 +0530

    Fix import case for OwnerItemCard component

--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 import { FaUtensils } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { FaPen } from "react-icons/fa";
-import OwnerItemCard from './ownerItemCard';
+import OwnerItemCard from './OwnerItemCard';
 
 function OwnerDashboard() {
   const { myShopData } = useSelector(state => state.owner)
