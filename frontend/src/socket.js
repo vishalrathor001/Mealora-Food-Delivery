31e94deb8575463d23307a8c9ec1603a2142859f
Vishal Kumar <145376154+vishalrathor001@users.noreply.github.com>
2026-04-06 06:11:02 +0530

    Change socket connection URL to production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000", {
+export const socket = io("https://mealora-backend.onrender.com", {
   autoConnect: false,        
   transports: ["websocket"], 
 });
